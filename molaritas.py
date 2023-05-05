2d6309a84277c4bb2756a30484dcdf770d120a9a
saviladrn <132325574+saviladrn@users.noreply.github.com>
2023-05-05 15:24:20 +0700

    Update molaritas.py

--- molaritas.py
+++ molaritas.py
@@ -1,3 +1,24 @@
 import streamlit as st
 
+st.title("Aplikasi Menghitung Molaritas")
+
+sampel = st.text_input("Masukkan nama bahan baku sekunder")
+penitar = st.text_input("Masukkan nama bahan baku primer")
+
+bobot = st.number_input("Masukkan nilai bobot zat bahan baku primer (gram)")
+volume = st.number_input("Masukkan volume larutan bahan baku sekunder (mL)")
+mr = st.number_input("Masukkan nilai Mr bahan baku primer (g/mol)")
+
+tombol = st.button("Hitung nilai Molaritas")
+
+if tombol:
+    molaritas=bobot/(mr*volume)
+    st.success(f'Nilai Molaritas adalah {molaritas}')
+    
+press=st.button("Tampilkan Kesimpulan")
+
+if press:
+    kesimpulan=bobot/(mr*volume)
+    st.write(f"Bahan baku sekunder",sampel,"yang telah distandarisasi oleh",penitar,"bernilai",kesimpulan,"mol/L")import streamlit as st
+
 st.write("Aplikasi Menghitung Molaritas")
